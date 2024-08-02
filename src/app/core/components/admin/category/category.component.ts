1094f1abc8d47e203a3b6b07da8eda9a386413e8
RKudtarkar <rohan.kudtarkar@zingworks.in>
2024-08-02 18:34:06 +0530

    added action buttons

--- src/app/core/components/admin/category/category.component.ts
+++ src/app/core/components/admin/category/category.component.ts
@@ -2,25 +2,17 @@ import { SharedModule } from '../../../../shared/shared.module';
 import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { ApiService } from '../../../../shared/services/api.service';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface CategoryModel {
+  CategoryId: number;
+  CategoryName: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const ELEMENT_DATA: CategoryModel[] = [
+  {CategoryId: 1, CategoryName: 'Hydrogen'},
+  {CategoryId: 1, CategoryName: 'Hydrogen'},
+  
 ];
 @Component({
   selector: 'app-category',
@@ -31,7 +23,11 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class CategoryComponent //implements AfterViewInit
 {
-  
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['CategoryId', 'CategoryName','action'];
   dataSource = ELEMENT_DATA;
+  //dataSource=this.apiService.getCategory().subscribe();
+  
+  
+  
 }
+
--- src/app/shared/components/page-side-nav/page-side-nav.component.ts
+++ src/app/shared/components/page-side-nav/page-side-nav.component.ts
@@ -8,6 +8,8 @@ export interface NavigationItem {
   link: string;
 }
 
+ 
+
 @Component({
   selector: 'page-side-nav',
   templateUrl: './page-side-nav.component.html',
@@ -16,35 +18,34 @@ export interface NavigationItem {
 export class PageSideNavComponent {
 
   panelName: string = ""
-  isAdmin: boolean=true;
+  isAdmin:boolean =true;
   navItems: NavigationItem[] = [];
 
   constructor(//private httpService: ApiService, 
     private router: Router) {
-      if(this.isAdmin)
-        {
-          this.panelName= "Admin"
-          this.navItems = [
-            { value: "Add Admin", link: "AddAdmin" },
-            { value: "Category", link: "Category" },
-            { value: "Subject", link: "Subject" },
-            { value: "Exam", link: "Exam" },
-            { value: "Question", link: "Question" },
-            { value: "result", link: "result" },
-            { value: "Student List", link: "StudentList" },
-            { value: "Admin Panel", link: "AdminPanel" },
-            { value: "Employee List", link: "EmployeeList" }
-          ]
-        }
-        else {
-          this.panelName= "User"
-          this.navItems = [
-            { value: "Exam", link: "Exam" },
-            { value: "Question", link: "Question" },
-            { value: "result", link: "result" },
-            
-          ]
-        }
+    if (this.isAdmin) {
+      this.panelName = "Admin"
+      this.navItems = [
+        { value: "Add Admin", link: "AddAdmin" },
+        { value: "Category", link: "Category" },
+        { value: "Subject", link: "Subject" },
+        { value: "Exam", link: "Exam" },
+        { value: "Question", link: "Question" },
+        { value: "result", link: "result" },
+        { value: "Student List", link: "StudentList" },
+        { value: "Admin Panel", link: "AdminPanel" },
+        { value: "Employee List", link: "EmployeeList" }
+      ]
+    }
+    else {
+      this.panelName = "User"
+      this.navItems = [
+        { value: "Exam", link: "Exam" },
+        { value: "Question", link: "Question" },
+        { value: "result", link: "result" },
+
+      ]
+    }
     router.navigateByUrl("/login");
     // httpService.userStatus.subscribe({
     //   next: (status) => {
@@ -62,7 +63,7 @@ export class PageSideNavComponent {
     //             { value: "User Approvals", link: "approval" },
     //             { value: "Book Master", link: "maintenance" },
     //             { value: "Return Book", link: "returnBook" },
-                
+
     //           ]
     //         } else if (user.userType == UserType.STUDENT) {
     //           this.panelName = 'Student Panel'
@@ -87,3 +88,14 @@ export class PageSideNavComponent {
   }
 
 }
+
+// function changeUser()
+// {
+//   if (isAdmin==true) {
+//     this.isAdmin=false;
+//   }
+//   else {
+//     this.isAdmin=true
+//   }
+
+// }
